9deb108a11b0223328033617890f954f125f77e1
Murtaza <murtaza58565452@gmail.com>
2021-07-30 14:05:51 +0500

    Added Superuser Final

--- Users/views.py
+++ Users/views.py
@@ -1,8 +1,6 @@
-from django.contrib.auth.forms import UsernameField
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import User as m_User
-from Books.models import Book as m_Book
 from .forms import User
 
 # Create your views here.
@@ -10,13 +8,13 @@ from .forms import User
 def signup(request):
     if request.method == 'POST':
         form = User(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.is_superuser = True
-            user.is_staff = True
-            user.save()
-            messages.success(request, f'Account created for {form.cleaned_data.get("first_name")}')
-            return redirect('login')
+        #if form.is_valid():
+        user = form.save(commit=False)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        #messages.success(request, f'Account created for {form.cleaned_data.get("first_name")}')
+        return redirect('login')
     else:
         form = User()
     return render(request, 'Users/signup.html', {'form': form })
